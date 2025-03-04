5045ea481d2b9dbb02b3ae053884def738e86036
Kiran Mahajan <kiran.mahajan@ansrsource.com>
2025-03-04 13:10:37 +0530

    fix: paths

--- backend/src/utils/dirname.js
+++ backend/src/utils/dirname.js
@@ -2,14 +2,22 @@ import { fileURLToPath } from "url";
 import { dirname, join, resolve } from "path";
 import fs from "fs";
 
-export const __dirname = dirname(dirname(fileURLToPath(import.meta.url)));
-export const uploadsDir = join(__dirname, "uploads");
-export const convertedDir = join(__dirname, "converted");
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
+export const backendSrcDir = __dirname;
+
+export const backendDir = dirname(backendSrcDir);
+
+export const rootDir = dirname(backendDir);
+
+export const frontendDistDir = resolve(rootDir, "frontend", "dist");
+
+export const uploadsDir = join(backendSrcDir, "uploads");
+export const convertedDir = join(backendSrcDir, "converted");
 
 [uploadsDir, convertedDir].forEach((dir) => {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
   }
 });
-export const rootDir = dirname(backendDir);
-export const frontendDistDir = resolve(rootDir, "frontend", "dist");
